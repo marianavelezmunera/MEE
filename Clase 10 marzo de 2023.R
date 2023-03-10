94dd090e6ecccc5c0a8781d673b00193d9bdce91
Mariana <marianavelez.35@gmail.com>
2023-03-10 11:32:18 -0600

    commit

--- Clase 10 marzo de 2023.R
+++ Clase 10 marzo de 2023.R
@@ -1,1 +1,15 @@
 # Clase 10 de marzo de 2023
+Murcis$Polen.Fecas<-NULL
+Murcis$observaciones<-NULL
+Murcis$ID_formato<-NULL
+Murcis$Hora<-NULL
+names(Murcis)
+unique(Murcis$Especie)
+str(Murcis) #para mirar la estructura de la base de datos 
+
+#cambiar a factor los caracteres
+
+Murcis$Localidad<-as.factor(Murcis$Localidad)
+Murcis$Encargado<-as.factor(Murcis$Encargado)
+Murcis$Especie<-as.factor(Murcis$Especie)
+Murcis$Sexo<-as.factor(Murcis$Sexo)
