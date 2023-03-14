7472f3318b2b42f7077d84ff4f9f4800e43e2907
Mariana <marianavelez.35@gmail.com>
2023-03-14 11:32:27 -0600

    commit

---
+++ Clase 14 de marzo de 2023.R
@@ -0,0 +1,15 @@
+# Clase 14 de marzo de 2023
+#Paquetes 
+install.packages("gridExtra")
+install.packages("mice")
+install.packages("naniar")
+install.packages("tidyquant")
+
+# Llamar paquetes
+library(tidyverse)
+library(gridExtra)
+library(mice)
+library(naniar)
+library(tidyquant)
+
+## Datos 
